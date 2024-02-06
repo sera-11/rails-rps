dc460186dd0c4f24d3a72576109a561c727795b8
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-06 22:34:53 +0000

    defined methods for paper and scissor pages and play_rock.html.erb to work for all 3 methods

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,10 +1,44 @@
 class GameController  < ApplicationController
   def rock
 
+    @our_move = "rock"
+
     @random_move = ["rock", "paper", "scissors"].sample
     if @random_move == "paper" 
       @output = "We lost!"
     elsif @random_move == "scissors"
+      @output = "We won!"
+    else
+      @output = "We tied!"
+    end
+    render({ :template => "game_templates/play_rock" })
+
+  end
+
+  def paper
+
+    @our_move = "paper"
+
+    @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock" 
+      @output = "We won!"
+    elsif @random_move == "scissors"
+      @output = "We lost!"
+    else
+      @output = "We tied!"
+    end
+    render({ :template => "game_templates/play_rock" })
+
+  end
+
+  def scissors
+
+    @our_move = "scissors"
+
+    @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock" 
+      @output = "We lost!"
+    elsif @random_move == "paper"
       @output = "We won!"
     else
       @output = "We tied!"
