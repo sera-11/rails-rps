1a9e9e53e82e45c64cc1c98ca32bee8c4a9b2379
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-06 21:46:11 +0000

    using render/ application controller

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,7 @@
+class ZebraController  < ApplicationController
+  def giraffe
+
+    render({ :template => "game_templates/play_rock" })
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 
+  get("/rock", { :controller => "zebra", :action => "giraffe" }) #controller=class, action=method
+
 end
